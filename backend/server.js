dfa4c1268121b54fadaadedfa7e4939cbf1f6600
maldhan.adhav123 <madhav@kennovation-services.com>
2024-07-24 16:26:07 +0530

    first fullstack created

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+import express from 'express';
+ const app = express()
+
+/* app.get('/', (req,res) => {
+    res.send('server is ready');
+ });
+*/
+ app.get('/api/jokes', (req, res) => {
+
+ const jokes = [
+        {
+            id: 1,
+            title: 'Why don’t scientists trust atoms?',
+            content: 'Because they make up everything!'
+        },
+        {
+            id: 2,
+            title: 'Why did the scarecrow win an award?',
+            content: 'Because he was outstanding in his field!'
+        },
+        {
+            id: 3,
+            title: 'What do you call fake spaghetti?',
+            content: 'An impasta!'
+        },
+        {
+            id: 4,
+            title: 'Why don’t skeletons fight each other?',
+            content: 'They don’t have the guts.'
+        },
+        {
+            id: 5,
+            title: 'What do you get when you cross a snowman and a vampire?',
+            content: 'Frostbite!'
+        }
+    ];
+    res.send(jokes);
+
+});
+ const port = process.env.PORT || 3000;
+ app.listen(port, () => {
+    console.log(`serve at http://localhost:${port}`)
+ });
+
